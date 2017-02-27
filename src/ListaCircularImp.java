3f6af7fffcfbef352abf8e30d64606f899837122
LuisDiegoFV <fer16344@uvg.edu.gt>
2017-02-26 22:01:46 -0600

    segundo

---
+++ src/ListaCircularImp.java
@@ -0,0 +1,110 @@
+package Listas;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class ListaCircularImp implements ListaInterface {
+	private int calculado;
+	private StackCircular stack;
+
+	@Override
+	public int method() throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		String cadena;
+		
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("js");
+		
+		//Se lee el archivo datos.txt
+		FileReader lecT = new FileReader(System.getProperty("user.dir") + "/datos.txt");
+		BufferedReader buff = new BufferedReader(lecT);
+		
+		stack = new StackCircular();
+		calculado = 0;
+		StackCircular momentaryStack = new StackCircular();
+		
+		//Ciclo while para meter los valores a utilizar, incluyendo los enteros como los operandos.
+        while((cadena = buff.readLine())!=null) {
+        	int n = 0;
+      
+            while(n < cadena.length()){
+            	int digits = 1;
+            	
+            	if (!cadena.substring(n, n+1).equals(" ")){
+            		
+            		try{
+            			while (!cadena.substring(n + digits, n + digits + 1).equals(" ")){
+            				digits++;
+            			}
+            			
+            		} catch (Exception e){
+            			
+            		}
+            		
+            	}
+            	
+            	stack.Push((cadena.substring(n, n + digits)));
+            	n = n + digits;
+            }
+            
+        }
+        
+        buff.close();
+		
+        try {
+			calculado =  (int) engine.eval(operando());
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+		};
+		
+		
+		return calculado;
+	}
+	
+	public String operando(){
+		boolean numeros = true;
+		String operador = stack.Get();
+		String resultado = "";
+		stack.Pop();
+		
+		while (numeros){
+			try {
+				
+				if (!stack.Get().equals(" ")) {
+					
+					resultado = resultado + operador.toString() + Integer.parseInt(stack.Get());
+					stack.Pop();
+					
+				}
+				
+				else {
+					stack.Pop();
+				}
+				
+			} catch (Exception e){
+				if (stack.Get() != " "){
+					
+					operador = stack.Get();
+					stack.Pop();
+		
+				}
+				
+				if (stack.Get() == ""){
+					
+					numeros = false;
+		
+				}
+			
+				
+			}
+		}
+		
+		return resultado.substring(1);
+	}
+
+}
